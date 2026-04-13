38cb16d28969dd8a09308b2bfd8d9257ebec1ea7
kimsgun <rizzieang@gmail.com>
2026-04-13 10:56:58 +0900

    1.0.29

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,26 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 import 'package:kakao_flutter_sdk_common/kakao_flutter_sdk_common.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 
 import 'app/app.dart';
 
+
+
+
+
+
+
+
+
+
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  KakaoSdk.init(nativeAppKey: ''); //카카오 api 네이티브 키
+await Firebase.initializeApp();
+KakaoSdk.init(nativeAppKey: '200f96c21fe1225e0b81fbe6e410012e'); //카카오 api 네이티브 키
 
-  FlutterBluePlus.setLogLevel(LogLevel.verbose, color: true);
+FlutterBluePlus.setLogLevel(LogLevel.verbose, color: true);
 
-  runApp(const MyApp());
+runApp(const MyApp());
 }
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,22 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import firebase_auth
+import firebase_core
+import flutter_blue_plus_darwin
+import google_sign_in_ios
+import shared_preferences_foundation
+import webview_flutter_wkwebview
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FlutterBluePlusPlugin.register(with: registry.registrar(forPlugin: "FlutterBluePlusPlugin"))
+  FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
+}
